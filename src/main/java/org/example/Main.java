c1fb8caee203d7f329bcbcedc3e3bde4ccf10989
mnaeem <mnaeem@gmail.com>
2023-06-08 19:10:20 +0500

    updated code

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
 
         //ChromeOptions co = new ChromeOptions();
-     //   co.addArguments("--remote-allow-origins=*");
+
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://demo.guru99.com/test/newtours/");
